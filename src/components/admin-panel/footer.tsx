d4cd23ba95aa618d56917d62b3a0717440d20475
DaviCunhaQ <davicunha1983@gmail.com>
2025-03-31 09:35:24 -0300

    feat: add GitHub icon and company name to footer component

--- src/components/admin-panel/footer.tsx
+++ src/components/admin-panel/footer.tsx
@@ -1,11 +1,21 @@
+import { AuthContext } from "@/context/authContext";
+import { useContext } from "react";
+import Github from "@/../public/git.svg";
 
 export function Footer() {
+  const {company} = useContext(AuthContext)
   return (
     <div className="z-20 w-full bg-background/95 shadow backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="mx-4 md:mx-8 flex h-14 items-center">
+      <div className="mx-4 md:mx-8 flex h-14 items-center justify-between">
         <p className="text-xs md:text-sm leading-loose text-muted-foreground text-left">
-          
+          Sistema de Ordens de Serviço - Empresa: {company}
         </p>
+        <a href="https://github.com/DaviCunhaQ" target="_blank" className="hover:cursor-pointer flex items-center gap-3 text-muted-foreground">
+          <p className="text-xs md:text-sm leading-loose text-muted-foreground text-left">
+            Feito por: DaviCunhaQ 
+          </p>
+          <img src={Github} alt="github" className="text-muted-foreground h-5" />
+        </a>
       </div>
     </div>
   );
