7e3247d11322542905ae26dba3f2643247bc118e
DaviCunhaQ <davicunha1983@gmail.com>
2025-04-23 23:55:04 -0300

    fix: improve zod error message

--- src/dtos/responseAxiosDto.ts
+++ src/dtos/responseAxiosDto.ts
@@ -1,9 +1,17 @@
 import { AxiosError } from "axios";
 
+export interface errorComplement {
+  code: string,
+  expected: string,
+  received: string,
+  path: string[],
+  message: string
+}
+
 export type ResponseAxiosDto<T> = {
   statusCode: number;
   body?: T;
   message?: string;
 };
 
-export type ErrorAxiosDto = AxiosError<{ message: string }>;
+export type ErrorAxiosDto = AxiosError<{ message: string , errors?: string}>;
--- src/hooks/order-services/useCreateOrderService.tsx
+++ src/hooks/order-services/useCreateOrderService.tsx
@@ -1,10 +1,25 @@
 "use client";
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { ErrorAxiosDto, ResponseAxiosDto, ServiceOrderFormOneSchema } from "../../dtos";
+import { ErrorAxiosDto, errorComplement, ResponseAxiosDto, ServiceOrderFormOneSchema } from "../../dtos";
 import { api } from "../../services/api";
 import toast from 'react-hot-toast'
 import { orderServiceQueryKeys } from "./order-service-query-keys";
+
+function generateToastByErrors( errors: errorComplement[] ){
+  const expireNumberOfErrors = errors.length > 3
+  let finalMessage = ""
+  if(expireNumberOfErrors){
+    finalMessage = "3 ou mais campos inválidos."
+  }else{
+    const errosWithoutFilter = errors.map((item)=>item.path[0])
+    const errorsList =  errosWithoutFilter.filter((error)=>error!=="cep")
+    finalMessage = `Erro nos campos: ${errorsList[0]}${errorsList[1] ? `, ${errorsList[1]}` : ""}${errorsList[2] ? `, ${errorsList[2]}` : ""}`
+  }
+
+  return finalMessage
+  
+}
 
 interface ResponseAxiosOccurrence extends ResponseAxiosDto<{ id: string , message: string}>{
   id: string;
@@ -28,6 +43,13 @@ export function useCreateOrderService(){
     onError: (data: ErrorAxiosDto) => {
       const message = data.response?.data.message || "Ocorreu um erro ao adicionar estes dados.";
       toast.error(message);
+      setTimeout(()=>{
+        if(data.response?.data.errors){
+          const errors = JSON.parse(data.response?.data.errors)
+          const messageErrors = generateToastByErrors(errors)
+          toast.error(messageErrors)
+        }
+      },300)
     },
     onSettled: async () => {
       await queryClient.invalidateQueries({
