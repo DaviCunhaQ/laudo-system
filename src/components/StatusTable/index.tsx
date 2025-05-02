b82c5c098b0dc8db5332c37fedb6f3a8cef85989
DaviCunhaQ <davicunha1983@gmail.com>
2025-05-02 16:40:20 -0300

    fix: think this is final test table

--- src/components/StatusTable/index.tsx
+++ src/components/StatusTable/index.tsx
@@ -96,11 +96,11 @@ const ListCell = ({
 };
 
 const ListColumn = ({ title, items, data }: { title: string; items: string[]; data?: any[] }) => (
-  <div className="flex flex-col min-w-[12rem] w-full max-w-[18rem] border-r border-black bg-gray-200 h-auto">
+  <div className="flex flex-col min-w-[12rem] w-full max-w-[18rem] border-r border-black bg-gray-200 h-max">
     <div className="bg-gray-600 text-white text-center font-semibold py-2 px-1 text-sm truncate">
       {title}
     </div>
-    <div className="flex-1 h-auto">
+    <div className="flex-1 h-max">
       {items.map((content, idx) => (
         <ListCell
           key={idx}
